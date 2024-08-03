1a0104b16b216dba43777d8892a2168be98f4e92
TENSHKUMAR K <tenshkumarkkt@gmail.com>
2024-08-03 16:32:24 +0530

    get messages b/w two persons and set the messages as readed

--- sparkwave-server/routes/messageRoute.js
+++ sparkwave-server/routes/messageRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
-const { addMessages } = require('../controllers/messagesController')
+const { addMessages, getMessages } = require('../controllers/messagesController')
 
 const router = express.Router()
 
 router.route('/send-message').post(addMessages)
+router.route('/get-messages/:from/:to').get(getMessages)
 
 module.exports = router
