16fb34073b885a80928297efd57f960394ebbd7c
TENSHKUMAR K <tenshkumarkkt@gmail.com>
2024-08-05 11:55:49 +0530

    Call communication socket setup done in server

--- sparkwave-client/src/components/call/VideoCall.jsx
+++ sparkwave-client/src/components/call/VideoCall.jsx
@@ -1,12 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import { useStateProvider } from '@/context/stateContext'
-const Container = dynamic(()=> import('./Container'),{ssr:false})
+const Container = dynamic(() => import('./Container'), { ssr: false })
 
 export default function VideoCall() {
-    const [{videoCall,socket,userInfo}] = useStateProvider()
+    const [{ videoCall, socket, userInfo }] = useStateProvider()
 
-  return ( 
-    <Container data={videoCall} />
-  )
+    useEffect(() => {
+        if(voiceCall.type === "out-going"){
+            socket.current.emit("outgoing-voice-call",{
+                to: voiceCall.id,
+                from:{ 
+                id: userInfo.id,
+                profilePicture: userInfo.profile_picture,
+                name: userInfo.name
+                },
+                callType: voiceCall.callType,
+                roomId: voiceCall.roomId
+            }
+        )
+        }
+    }, [voiceCall])
+
+    return (
+        <Container data={videoCall} />
+    )
 }
