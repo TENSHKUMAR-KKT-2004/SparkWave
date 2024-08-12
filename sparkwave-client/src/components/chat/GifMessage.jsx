99e8de5734745c1ccbb122c12b5fb9c3c3f512f5
TENSHKUMAR K <tenshkumarkkt@gmail.com>
2024-08-12 10:38:30 +0530

    Hostname changed

--- sparkwave-client/src/components/chat/GifMessage.jsx
+++ sparkwave-client/src/components/chat/GifMessage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStateProvider } from '@/context/stateContext'
+import Image from 'next/image'
 import { calculateTime } from '@/utils/calculateTime'
 import MessageStatus from '../common/MessageStatus'
 
@@ -9,7 +10,7 @@ export default function GifMessage({ message }) {
     return (
         <div className={`p-1 rounded-lg ${message.senderId === currentChatUser.id ? 'bg-incoming-background' : 'bg-outgoing-background'}`}>
             <div className="relative">
-                <img src={message.message} alt="gif" className="rounded-lg" height={300} width={300} />
+                <Image src={message.message} alt="gif" className="rounded-lg" height={300} width={300} />
                 <div className="absolute bottom-1 right-1 flex items-end gap-1">
                     <span className="text-bubble-meta text-[11px] pt-1 min-w-fit">
                         {
